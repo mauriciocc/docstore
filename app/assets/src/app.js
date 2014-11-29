33a2567bf39850601b567adbcd2ee520a377281d
Mauricio Colognese Concatto <mauriciocc@outlook.com>
2014-11-29 09:17:41 -0200

    Ajax indicator

--- app/assets/src/app.js
+++ app/assets/src/app.js
@@ -122,6 +122,25 @@ angular.module("docstore", [
             }
         };
     })
+    .directive('loading', function ($http) {
+        return {
+            restrict: 'A',
+            link: function (scope, elm, attrs) {
+                scope.isLoading = function () {
+                    return $http.pendingRequests.length > 0;
+                };
+
+                scope.$watch(scope.isLoading, function (v) {
+                    if (v) {
+                        elm.show();
+                    } else {
+                        elm.hide();
+                    }
+                });
+            }
+        };
+
+    })
     .run(function ($rootScope, $location, $cookies, $route, Page, Auth) {
         $rootScope.$on("$routeChangeStart", function (event, next, current) {
             Page.setLoginPage(!!next.loginPage);
